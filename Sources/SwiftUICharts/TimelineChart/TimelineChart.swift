231ca70b05f2f8ab4902e8680f48831b7a1e88c3
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-07-24 15:06:52 +0900

    Assorted cleanup and cuts

--- Sources/SwiftUICharts/TimelineChart/TimelineChart.swift
+++ Sources/SwiftUICharts/TimelineChart/TimelineChart.swift
@@ -10,83 +10,59 @@ import SwiftUI
 
 public struct TimelineChart: View {
     @ObservedObject var data: ChartData
-    public var title: String?
-    public var legend: String?
     public var style: ChartStyle
-    
+
     @Environment(\.colorScheme) var colorScheme: ColorScheme
-    @State private var showLegend = false
+
     @State private var dragLocation:CGPoint = .zero
     @State private var indicatorLocation:CGPoint = .zero
     @State private var closestPoint: CGPoint = .zero
     @State private var opacity:Double = 0
     @State private var currentDataNumber: Double = 0
-    @State private var currentDataString: String = "Format here"
+    @State private var currentDataString: String = ""
     @State private var hideHorizontalLines: Bool = false
     
     public var body: some View {
         GeometryReader{ geometry in
-            VStack(alignment: .leading, spacing: 8) {
-                Group{
-                    if (self.title != nil){
-                        Text(self.title!)
-                            .font(.title)
-                            .bold().foregroundColor(self.style.textColor)
-                    }
-                    if (self.legend != nil){
-                        Text(self.legend!)
-                            .font(.callout)
-                            .foregroundColor(self.style.legendTextColor)
-                    }
-                }.offset(x: 0, y: 20)
-                ZStack{
-                    GeometryReader{ reader in
-                        Rectangle()
-                            .foregroundColor(self.style.backgroundColor)
-                        if(self.showLegend){
-                            Legend(data: self.data,
-                                   frame: .constant(reader.frame(in: .local)), hideHorizontalLines: self.$hideHorizontalLines)
-                                .transition(.opacity)
-                                .animation(Animation.easeOut(duration: 1).delay(1))
-                        }
-                        Line(data: self.data,
-                             frame: .constant(CGRect(x: 0, y: 0, width: reader.frame(in: .local).width - 30, height: reader.frame(in: .local).height)),
-                             touchLocation: self.$indicatorLocation,
-                             showIndicator: self.$hideHorizontalLines,
-                             minDataValue: .constant(nil),
-                             maxDataValue: .constant(nil),
-                             showBackground: false,
-                             gradient: self.style.gradientColor
-                        )
-                        .offset(x: 30, y: -20)
-                        .onAppear(){
-                            self.showLegend = true
-                        }
-                        .onDisappear(){
-                            self.showLegend = false
-                        }
-                    }
-                    .frame(width: geometry.frame(in: .local).size.width, height: 240)
-                    .offset(x: 0, y: 40 )
-                    HoverView(currentNumber: self.$currentDataNumber, currentLabel:  self.$currentDataString, scheme: style)
-                        .opacity(self.opacity)
-                        .offset(x: self.dragLocation.x - geometry.frame(in: .local).size.width/2, y: 36)
+            ZStack{
+                GeometryReader{ reader in
+                    Legend(data: self.data,
+                           frame: .constant(reader.frame(in: .local)), hideHorizontalLines: self.$hideHorizontalLines)
+                        .transition(.opacity)
+                        .animation(Animation.easeOut(duration: 1).delay(1))
+                    
+                    Line(data: self.data,
+                         frame: .constant(CGRect(x: 0, y: 0, width: reader.frame(in: .local).width - 30, height: reader.frame(in: .local).height)),
+                         touchLocation: self.$indicatorLocation,
+                         showIndicator: self.$hideHorizontalLines,
+                         minDataValue: .constant(nil),
+                         maxDataValue: .constant(nil),
+                         showBackground: false,
+                         gradient: self.style.gradientColor
+                    )
+                    .offset(x: 30, y: -20)
                 }
                 .frame(width: geometry.frame(in: .local).size.width, height: 240)
-                .gesture(DragGesture()
-                            .onChanged({ value in
-                                self.dragLocation = value.location
-                                self.indicatorLocation = CGPoint(x: max(value.location.x-30,0), y: 32)
-                                self.opacity = 1
-                                self.closestPoint = self.getClosestDataPoint(toPoint: value.location, width: geometry.frame(in: .local).size.width-30, height: 240)
-                                self.hideHorizontalLines = true
-                            })
-                            .onEnded({ value in
-                                self.opacity = 0
-                                self.hideHorizontalLines = false
-                            })
-                )
+                .offset(x: 0, y: 40 )
+                HoverView(currentNumber: self.$currentDataNumber, currentLabel:  self.$currentDataString, scheme: style)
+                    .opacity(self.opacity)
+                    .offset(x: self.dragLocation.x - geometry.frame(in: .local).size.width/2, y: 36)
             }
+            .frame(width: geometry.frame(in: .local).size.width, height: 240)
+            .gesture(DragGesture()
+                        .onChanged({ value in
+                            self.dragLocation = value.location
+                            self.indicatorLocation = CGPoint(x: max(value.location.x-30,0), y: 32)
+                            self.opacity = 1
+                            self.closestPoint = self.getClosestDataPoint(toPoint: value.location, width: geometry.frame(in: .local).size.width-30, height: 240)
+                            self.hideHorizontalLines = true
+                        })
+                        .onEnded({ value in
+                            self.opacity = 0
+                            self.hideHorizontalLines = false
+                        })
+            )
+            
         }
     }
     
@@ -97,7 +73,8 @@ public struct TimelineChart: View {
         
         let index:Int = Int(floor((toPoint.x-15)/stepWidth))
         if (index >= 0 && index < points.count){
-            self.currentDataNumber = points[index]
+            self.currentDataString = data.points[index].0
+            self.currentDataNumber = data.points[index].1
             return CGPoint(x: CGFloat(index)*stepWidth, y: CGFloat(points[index])*stepHeight)
         }
         return .zero
@@ -125,10 +102,8 @@ public struct HoverView: View {
 }
 
 extension TimelineChart {
-    public init(title: String, data: ChartData) {
+    public init(data: ChartData) {
         self.data = data
-        self.title = title
-        self.legend = "Legend"
         self.style = Styles.lineChartStyleOne
     }
 }
@@ -136,6 +111,6 @@ extension TimelineChart {
 
 struct TimelineChart_Previews: PreviewProvider {
     static var previews: some View {
-        TimelineChart(title: "Test", data: ChartData(values: [("test", 1)]))
+        TimelineChart(data: ChartData(values: [("test", 1)]))
     }
 }
